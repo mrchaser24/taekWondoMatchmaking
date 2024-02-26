0380fd85a5b13d8c8a8425137662cd335d359d04
Chase Arcos HERMOSISIMA <chase.hermosisima@ext.essilor.com>
2024-02-26 16:40:10 +0800

    adding Update BASE to REPO

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/taekWondoMatchmaking/Student',
+  base: '/taekWondoMatchmaking/',
   plugins: [vue()],
   resolve: {
     alias: {
